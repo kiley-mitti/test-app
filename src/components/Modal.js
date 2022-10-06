3a21751be083c86b8d70271ba53f1e4a54c9780d
Kiley Mitti <kiley.mitti@rocketcom.com>
2022-10-06 15:13:12 -0400

    tweak dialog functionality

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import { RuxDialog, RuxButton } from '@astrouxds/react';
+import { RuxDialog } from '@astrouxds/react';
 import { useGlobalContext } from '../context';
+import { useEffect } from 'react';
 
 const Modal = () => {
   //get context stuff as needed
-  const { modalInfo, setModalInfo, modalInitial } = useGlobalContext();
+  const { modalInfo, acknowledge, setModalInfo, modalInitial } = useGlobalContext();
+  console.log(modalInfo);
+  const parseEvent = (e) => {
+    if (e.detail) {
+      acknowledge(modalInfo.contactId);
+    } else {
+      setModalInfo(modalInitial);
+    }
+  };
+  useEffect(() => {
+    const modal = document.querySelector('rux-dialog');
+    modal.addEventListener('ruxdialogclosed', parseEvent);
+
+    return () => {
+      modal.removeEventListener('ruxdialogclosed', parseEvent);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [modalInfo]);
+
   return (
-    <RuxDialog open={modalInfo.open ? true : false} message={`Message: ` + modalInfo.satDetail} header={`Satallite: ` + modalInfo.satName}>
-      <span slot='footer'>
-        <RuxButton onClick={() => setModalInfo({ modalInitial })}>Ok</RuxButton>
-      </span>
-    </RuxDialog>
+    <>
+      <RuxDialog open={modalInfo.open ? true : false} message={`Message: ` + modalInfo.satDetail} header={`Satallite: ` + modalInfo.satName} confirm-text='Acknowledge' deny-text='Cancel'></RuxDialog>
+    </>
   );
 };
 export default Modal;
